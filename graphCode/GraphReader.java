6b443aaa9966236b854f75c644b4b41d2c35e82b
Brian Nguyen <briannguyen636@gmail.com>
2024-11-21 13:56:44 -0800

    GraphReader working for all graph types

--- graphCode/GraphReader.java
+++ graphCode/GraphReader.java
@@ -2,17 +2,29 @@ import java.io.BufferedReader;
 
 public class GraphReader {
 
-    public static SimpleGraph graphReader(String filename) {
+    public static SimpleGraph graphReader(String filepath) {
         SimpleGraph graph = new SimpleGraph();
+        String path = "graphGenerationCode/graphGenerationCode/";
+        String graphType = filepath.split("/")[0];
 
-        try (BufferedReader reader = InputLib.fopen(filename)){
-
+        try (BufferedReader reader = InputLib.fopen(path + filepath)){
             while (reader.ready()) {
                 String line = reader.readLine();
-                String[] split = line.split("\t");
-                String vertex1 = split[1];
-                String vertex2 = split[2];
-                int value = Integer.parseInt(split[3]);
+                String vertex1, vertex2;
+                int value;
+
+                if (graphType.equals("Bipartite")) {
+                    String[] split = line.split("\t");
+                    vertex1 = split[1];
+                    vertex2 = split[2];
+                    value = Integer.parseInt(split[3]);
+                } else {
+                    String[] split = line.split(" ");
+                    vertex1 = split[0];
+                    vertex2 = split[1];
+                    value = Integer.parseInt(split[2]);
+                }
+
                 //Get the two vertexes
                 Vertex v1;
                 if (!graph.containsVertex(vertex1)) {
@@ -29,7 +41,7 @@ public class GraphReader {
 
                 graph.insertEdge(v1, v2,
                         new int[]{0,0,value},
-                        vertex1 + "," + vertex2);
+                        vertex1 + "-" + vertex2);
             }
         } catch (Exception e) {
             System.out.println(e);
@@ -38,7 +50,11 @@ public class GraphReader {
     }
 
     public static void main(String[] args) {
-        SimpleGraph g = graphReader("graphs/g2.txt");
+        SimpleGraph g;
+//        g = graphReader("Bipartite/g1.txt");
+//        g = graphReader("FixedDegree/20v-3out-4min-355max.txt");
+        g = graphReader("Mesh/smallMesh.txt");
+//        g = graphReader("Random/n10-m10-cmin5-cmax10-f30.txt");
         System.out.println("Vertices: " + g.numVertices());
         System.out.println("Edges: " + g.numEdges());
     }
