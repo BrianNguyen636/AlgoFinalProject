64212602638c6a9eb228095955f8f45e6e59241e
Brian Nguyen <briannguyen636@gmail.com>
2024-11-22 16:54:07 -0800

    Fixed augmenting path return list

--- graphCode/FordFulkerson.java
+++ graphCode/FordFulkerson.java
@@ -38,45 +38,55 @@ public class FordFulkerson {
 
 
         }
-
-        System.out.println("Vertices: " + graph.numVertices());
-        System.out.println("Edges: " + graph.numEdges());
-
-
         return residual;
     }
 
-    public static List<Vertex> augment(SimpleGraph residual) {
+    public static List<Vertex> augmentingPath(SimpleGraph residual) {
         List<Vertex> path = new LinkedList<>();
         if (dfsAugment(residual.aVertex(), path)) return path;
         else return null;
     }
     public static boolean dfsAugment(Vertex v, List<Vertex> path) {
+        System.out.println("Visiting:" + v.getName());
         path.add(v);
-        if (v.getName().equals("t")) {
-            return true;
-        }
+        if (v.getName().equals("t")) return true;
+
         for (Object e : v.incidentEdgeList) {
             Edge edge = (Edge)e;
             Vertex v1 = edge.getFirstEndpoint();
             Vertex v2 = edge.getSecondEndpoint();
             if (v1 == v) {
                 EdgeData data = (EdgeData) edge.getData();
-                if (data.flow < data.capacity && !path.contains(v2)) {
+                System.out.println("Edge: " + edge.getName());
+                System.out.println("Flow:"+ (int)(data.capacity - data.flow));
+                if (data.capacity - data.flow > 0 && !path.contains(v2)) {
                     if (dfsAugment(v2, path)) {
                         return true;
                     }
                 }
             }
         }
+        //If vertex does not lead to the target
+        path.removeLast();
         return false;
     }
 
+    public static int augment(List<Vertex> path, SimpleGraph graph, SimpleGraph residual) {
+        int flow = 0;
+
+        return flow;
+    }
+
     public static void main(String[] args) {
-        SimpleGraph residual = buildResidual(GraphReader.readGraph("test.txt"));
+        SimpleGraph graph = GraphReader.readGraph("test2.txt");
+        SimpleGraph residual = buildResidual(graph);
+
+        System.out.println("Vertices: " + graph.numVertices());
+        System.out.println("Edges: " + graph.numEdges());
         System.out.println("Vertices:" + residual.numVertices());
         System.out.println("Edges:" + residual.numEdges());
-        List<Vertex> augment = augment(residual);
+
+        List<Vertex> augment = augmentingPath(residual);
         System.out.println(augment);
     }
 }
