3f75395eeb879c211bf5aa8136cb3ac76a37dcfb
Brian Nguyen <briannguyen636@gmail.com>
2024-11-23 17:30:33 -0800

    algorithm working on large fixed degree

--- graphCode/FordFulkerson.java
+++ graphCode/FordFulkerson.java
@@ -34,13 +34,11 @@ public class FordFulkerson {
                 new EdgeData(0, eData.capacity-eData.flow),
                 v1.getName() + "-" + v2.getName()
             );
-            //Backwards edge
+
             residual.insertEdge(v2res, v1res,
                     new EdgeData(0, eData.flow),
                     v2.getName() + "-" + v1.getName()
             );
-
-
         }
         return residual;
     }
@@ -48,34 +46,24 @@ public class FordFulkerson {
     /**
      * Finds an augmenting path and returns it or null if none
      * @param residual The residual graph
-     * @return List of vertices for the augmenting path
+     * @return List of Edges for the augmenting path
      */
-    public static List<Vertex> augmentingPath(SimpleGraph residual) {
-        List<Vertex> path = new LinkedList<>();
+    public static List<Object> augmentingPath(SimpleGraph residual) {
+        List<Object> path = new LinkedList<>();
         Vertex source = residual.aVertex();
-        path.add(source);
         Set<Vertex> visited = new HashSet<>();
         visited.add(source);
         if (dfsAugment(source, path, visited)) return path;
         else return null;
-
-//        List<String> strPath = bfsAugment(residual);
-//        if (strPath == null) return null;
-//        for (String s : strPath) {
-//            path.add(residual.vertexMap.get(s));
-//        }
-//        return path;
     }
 
     /**
      * Recursive helper for augmentingPath()
      * @param v vertex searched
-     * @param path the path built so far
+     * @param path the path of edges built so far
      * @return True or False the target can be reached.
      */
-    public static boolean dfsAugment(Vertex v, List<Vertex> path, Set<Vertex> visited) {
-//        System.out.println("Visiting:" + v.getName());
-
+    public static boolean dfsAugment(Vertex v, List<Object> path, Set<Vertex> visited) {
         if (v.getName().equals("t")) {
             return true;
         }
@@ -85,96 +73,44 @@ public class FordFulkerson {
             EdgeData data = (EdgeData) edge.getData();
             Vertex v1 = edge.getFirstEndpoint();
             Vertex v2 = edge.getSecondEndpoint();
-            if (v1 == v && data.capacity - data.flow > 0 && visited.add(v2)) {
-//                System.out.println("Edge: " + edge.getName());
-//                System.out.println("Flow:"+ (int)(data.capacity - data.flow));
-                path.add(v2);
+            if (v1 == v && data.flow < data.capacity && visited.add(v2)) {
+                path.add(e);
                 if (dfsAugment(v2, path, visited)) {
                     return true;
                 }
             }
         }
-        path.removeLast();
         return false;
     }
-    public static List<String> bfsAugment(SimpleGraph residual) {
-        Vertex v = residual.aVertex();
-        List<String> path = new LinkedList<>();
-        path.add((String)v.getName());
-        Queue<List<String>> queue = new LinkedList<>();
-        queue.add(path);
-        while (queue.size() != 0) {
-            List<String> p = queue.poll();
-            Vertex vert = residual.vertexMap.get(p.getLast());
-            if (vert.getName().equals("t")) {
-                return p;
-            }
-            for (Object e : vert.incidentEdgeList) {
-                Edge edge = (Edge)e;
-                Vertex v1 = edge.getFirstEndpoint();
-                Vertex v2 = edge.getSecondEndpoint();
-                if (v1 == vert) {
-                    EdgeData data = (EdgeData) edge.getData();
-//                System.out.println("Edge: " + edge.getName());
-//                System.out.println("Flow:"+ (int)(data.capacity - data.flow));
-                    if (data.capacity - data.flow > 0 && !p.contains((String)v2.getName())) {
-                        List<String> next = new LinkedList<>(p);
-                        next.add((String)v2.getName());
-                        queue.add(next);
-                    }
-                }
-            }
 
-        }
-        return null;
-    }
-    public static int findBottleneck(List<Vertex> path, SimpleGraph residual) {
+    public static int findBottleneck(List<Object> path, SimpleGraph residual) {
+
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < path.size() - 1; i++) {
-            Vertex v = path.get(i);
-
-            Edge edge = residual.edgeMap.get(v.getName() + "-" + path.get(i+1).getName());
+        for (int i = 0; i < path.size(); i++) {
+            Edge edge = (Edge)path.get(i);
             EdgeData data = (EdgeData) edge.getData();
             if (data.capacity < min) {
                 min = data.capacity;
             }
-
-//            for (Object e : v.incidentEdgeList) {
-//                Edge edge = (Edge) e;
-//                Vertex v1 = edge.getFirstEndpoint();
-//                Vertex v2 = edge.getSecondEndpoint();
-//                if (v1 == v && v2 == path.get(i + 1)) {
-//                    EdgeData data = (EdgeData) edge.getData();
-//                    if (data.capacity < min) {
-//                        min = data.capacity;
-//                    }
-//                }
-//            }
         }
         return min;
     }
     public static int augment(SimpleGraph graph, SimpleGraph residual) {
 
-        System.out.println("Finding augment");
-        List<Vertex> path = augmentingPath(residual);
+//        System.out.println("---Finding augment");
+        List<Object> path = augmentingPath(residual);
 
         if (path == null) return 0;
 
-//        System.out.print("Path: ");
-//        for (Vertex v : path) {
-//            System.out.print((","+v.getName()));
-//        }
-//        System.out.println();
-
-        System.out.println("Finding bottleneck");
+//        System.out.println("---Finding bottleneck");
         int flow = findBottleneck(path, residual);
         Vertex graphVertex = graph.aVertex();
 
-        System.out.println("Modifying graphs");
-        for (int i = 1; i < path.size(); i++) {
-            Vertex v = path.get(i);
-            String v1name = (String)graphVertex.getName();
-            String v2name = (String)v.getName();
+//        System.out.println("---Modifying graphs");
+        for (int i = 0; i < path.size(); i++) {
+            Edge edge = (Edge) path.get(i);
+            String v1name = (String)edge.getFirstEndpoint().getName();
+            String v2name = (String)edge.getSecondEndpoint().getName();
 
             //Graph
             Edge e;
@@ -197,10 +133,10 @@ public class FordFulkerson {
             EdgeData data2 = (EdgeData) resB.getData();
             resB.setData(new EdgeData(0, data2.capacity + flow));
 
-            graphVertex = v;
+//            graphVertex = v;
 
         }
-        System.out.println(flow + " flow added");
+//        System.out.println("---" + flow + " flow added");
         return flow;
     }
 
@@ -208,11 +144,10 @@ public class FordFulkerson {
 
         SimpleGraph graph = GraphReader.readGraph(filepath);
         SimpleGraph residual = buildResidual(graph);
-
         int maxflow = 0;
         int flow = augment(graph, residual);
         while (flow != 0) {
-            System.out.println("Flow:"+flow);
+//            System.out.println("Flow:"+flow);
             maxflow += flow;
 //            Thread.sleep(1000);
             flow = augment(graph, residual);
@@ -234,5 +169,40 @@ public class FordFulkerson {
 //        System.out.println(fordFulkerson("bipartite/g2.txt"));
 //        System.out.println(fordFulkerson("FixedDegree/20v-3out-4min-355max.txt"));
         System.out.println(fordFulkerson("FixedDegree/100v-5out-25min-200max.txt"));
+//        System.out.println(fordFulkerson("Mesh/smallMesh.txt"));
+//        System.out.println(fordFulkerson("Mesh/mediumMesh.txt"));
+//        System.out.println(fordFulkerson("Random/n10-m10-cmin5-cmax10-f30.txt"));
+//        System.out.println(fordFulkerson("Random/n100-m100-cmin10-cmax20-f949.txt"));
     }
 }
+//public static List<String> bfsAugment(SimpleGraph residual) {
+//    Vertex v = residual.aVertex();
+//    List<String> path = new LinkedList<>();
+//    path.add((String)v.getName());
+//    Queue<List<String>> queue = new LinkedList<>();
+//    queue.add(path);
+//    while (queue.size() != 0) {
+//        List<String> p = queue.poll();
+//        Vertex vert = residual.vertexMap.get(p.getLast());
+//        if (vert.getName().equals("t")) {
+//            return p;
+//        }
+//        for (Object e : vert.incidentEdgeList) {
+//            Edge edge = (Edge)e;
+//            Vertex v1 = edge.getFirstEndpoint();
+//            Vertex v2 = edge.getSecondEndpoint();
+//            if (v1 == vert) {
+//                EdgeData data = (EdgeData) edge.getData();
+////                System.out.println("Edge: " + edge.getName());
+////                System.out.println("Flow:"+ (int)(data.capacity - data.flow));
+//                if (data.capacity - data.flow > 0 && !p.contains((String)v2.getName())) {
+//                    List<String> next = new LinkedList<>(p);
+//                    next.add((String)v2.getName());
+//                    queue.add(next);
+//                }
+//            }
+//        }
+//
+//    }
+//    return null;
+//}
