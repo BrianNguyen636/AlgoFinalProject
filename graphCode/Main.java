03651c630441fee3460603dbd5888b87bb6b1922
OtherAndrew <andrewnguyen841@gmail.com>
2024-12-04 15:45:43 -0800

    generated more graphs of varying max capacities

--- graphCode/Main.java
+++ graphCode/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        final int ITERATIONS = 100;
+        final int ITERATIONS = 1;
         
         String[] tests = {
                 // "test.txt",
@@ -13,13 +13,32 @@ public class Main {
                 // "FixedDegree/100v-5out-25min-200max.txt",
                 // "Mesh/smallMesh.txt",
                 // "Mesh/mediumMesh.txt",
-                // "Random/n10-m10-cmin5-cmax10-f30.txt",
+                
+                // "Bipartite/100s_100t_0.5p_1min_1max.txt",
+                // "Bipartite/100s_100t_0.5p_1min_10max.txt",
+                // "Bipartite/100s_100t_0.5p_1min_50max.txt",
+                // "Bipartite/100s_100t_0.5p_1min_100max.txt",
+                // "Bipartite/100s_100t_0.5p_1min_500max.txt",
+                // "Bipartite/100s_100t_0.5p_1min_1000max.txt",
 
-                "Bipartite/100s_100t_0.5p_1min_10max.txt",
-                "Bipartite/100s_100t_0.5p_1min_50max.txt",
-                "FixedDegree/1000v_100e_1min_100max.txt",
-                "Mesh/100r_100c_1cap.txt",
-                "Random/n100-m100-cmin10-cmax20-f949.txt"
+                // "FixedDegree/1000v_100e_1min_1max.txt",
+                // "FixedDegree/1000v_100e_1min_10max.txt",
+                // "FixedDegree/1000v_100e_1min_50max.txt",
+                // "FixedDegree/1000v_100e_1min_100max.txt",
+                // "FixedDegree/1000v_100e_1min_500max.txt",
+                // "FixedDegree/1000v_100e_1min_1000max.txt",
+
+                // "Mesh/50r_50c_1cap.txt",
+                // "Mesh/50r_50c_10cap.txt",
+                // "Mesh/50r_50c_50cap.txt",
+                // "Mesh/50r_50c_100cap.txt",
+                // "Mesh/50r_50c_500cap.txt",
+                // "Mesh/50r_50c_1000cap.txt",
+
+                // "Mesh/100r_100c_1cap.txt",
+
+                // "Random/n10-m10-cmin5-cmax10-f30.txt",
+                // "Random/n100-m100-cmin10-cmax20-f949.txt"
         };
 
         long start = System.currentTimeMillis();
