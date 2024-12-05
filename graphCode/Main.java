d05f3a1307d7bec416c37a97aa0618fa68dd5b2f
OtherAndrew <andrewnguyen841@gmail.com>
2024-12-04 19:17:50 -0800

    add experimental results for 100 iterations on new graphs of varying max capacity

--- graphCode/Main.java
+++ graphCode/Main.java
@@ -1,8 +1,8 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final int ITERATIONS = 1;
+        final int ITERATIONS = 100;
         
         String[] tests = {
                 // "test.txt",
@@ -14,28 +14,26 @@ public class Main {
                 // "Mesh/smallMesh.txt",
                 // "Mesh/mediumMesh.txt",
                 
-                // "Bipartite/100s_100t_0.5p_1min_1max.txt",
-                // "Bipartite/100s_100t_0.5p_1min_10max.txt",
-                // "Bipartite/100s_100t_0.5p_1min_50max.txt",
-                // "Bipartite/100s_100t_0.5p_1min_100max.txt",
-                // "Bipartite/100s_100t_0.5p_1min_500max.txt",
-                // "Bipartite/100s_100t_0.5p_1min_1000max.txt",
+                "Bipartite/100s_100t_0.5p_1min_1max.txt",
+                "Bipartite/100s_100t_0.5p_1min_10max.txt",
+                "Bipartite/100s_100t_0.5p_1min_50max.txt",
+                "Bipartite/100s_100t_0.5p_1min_100max.txt",
+                "Bipartite/100s_100t_0.5p_1min_500max.txt",
+                "Bipartite/100s_100t_0.5p_1min_1000max.txt",
 
-                // "FixedDegree/1000v_100e_1min_1max.txt",
-                // "FixedDegree/1000v_100e_1min_10max.txt",
-                // "FixedDegree/1000v_100e_1min_50max.txt",
-                // "FixedDegree/1000v_100e_1min_100max.txt",
-                // "FixedDegree/1000v_100e_1min_500max.txt",
-                // "FixedDegree/1000v_100e_1min_1000max.txt",
+                "FixedDegree/1000v_100e_1min_1max.txt",
+                "FixedDegree/1000v_100e_1min_10max.txt",
+                "FixedDegree/1000v_100e_1min_50max.txt",
+                "FixedDegree/1000v_100e_1min_100max.txt",
+                "FixedDegree/1000v_100e_1min_500max.txt",
+                "FixedDegree/1000v_100e_1min_1000max.txt",
 
-                // "Mesh/50r_50c_1cap.txt",
-                // "Mesh/50r_50c_10cap.txt",
-                // "Mesh/50r_50c_50cap.txt",
-                // "Mesh/50r_50c_100cap.txt",
-                // "Mesh/50r_50c_500cap.txt",
-                // "Mesh/50r_50c_1000cap.txt",
-
-                // "Mesh/100r_100c_1cap.txt",
+                "Mesh/50r_50c_1cap.txt",
+                "Mesh/50r_50c_10cap.txt",
+                "Mesh/50r_50c_50cap.txt",
+                "Mesh/50r_50c_100cap.txt",
+                "Mesh/50r_50c_500cap.txt",
+                "Mesh/50r_50c_1000cap.txt",
 
                 // "Random/n10-m10-cmin5-cmax10-f30.txt",
                 // "Random/n100-m100-cmin10-cmax20-f949.txt"
@@ -44,13 +42,13 @@ public class Main {
         long start = System.currentTimeMillis();
         long totalTime = (System.currentTimeMillis() - start);
         long timeSum = 0;
-        double avgTime = 0;
 
         System.out.println("Taking average time of " + ITERATIONS + " iterations.");
         System.out.println();
                 
         for (String s : tests) {   
             System.out.println("Input graph: " + s);
+            double[] avgTimes = new double[3];
 
             timeSum = 0;
             for (int i = 0; i < ITERATIONS; i++) {
@@ -59,8 +57,8 @@ public class Main {
                 totalTime = (System.currentTimeMillis() - start);
                 timeSum += totalTime;
             }
-            avgTime = (double) timeSum / ITERATIONS;
-            System.out.println("- Ford-Fulkerson:\t" + avgTime + " ms");
+            avgTimes[0] = (double) timeSum / ITERATIONS;
+            
 
             timeSum = 0;
             for (int i = 0; i < ITERATIONS; i++) {
@@ -69,8 +67,8 @@ public class Main {
                 totalTime = (System.currentTimeMillis() - start);
                 timeSum += totalTime;
             }
-            avgTime = (double) timeSum / ITERATIONS;
-            System.out.println("- Capacity Scaling:\t" + avgTime + " ms");
+            avgTimes[1] = (double) timeSum / ITERATIONS;
+            
 
             timeSum = 0;
             for (int i = 0; i < ITERATIONS; i++) {
@@ -79,11 +77,33 @@ public class Main {
                 totalTime = (System.currentTimeMillis() - start);
                 timeSum += totalTime;
             }
-            avgTime = (double) timeSum / ITERATIONS;
-            System.out.println("- Preflow Push:\t\t" + avgTime + " ms");
+            avgTimes[2] = (double) timeSum / ITERATIONS;
+
+            double minTime = Arrays.stream(avgTimes).min().getAsDouble();
+            double maxTime = Arrays.stream(avgTimes).max().getAsDouble();
+            
+            System.out.print("- Ford-Fulkerson:\t" + avgTimes[0] + " ms");
+            fastestOrSlowest(avgTimes[0], minTime, maxTime);
+            System.out.println();
+
+            System.out.print("- Capacity Scaling:\t" + avgTimes[1] + " ms");
+            fastestOrSlowest(avgTimes[1], minTime, maxTime);
+            System.out.println();
+
+            System.out.print("- Preflow Push:\t\t" + avgTimes[2] + " ms");
+            fastestOrSlowest(avgTimes[2], minTime, maxTime);
+            System.out.println();
 
             System.out.println();
         }
         
+    }
+
+    public static void fastestOrSlowest(double time, double min, double max) {
+        if (time == min) {
+            System.out.print(" - (fastest)");
+        } else if (time == max) {
+            System.out.print(" - (slowest)");
+        }
     }
 }
